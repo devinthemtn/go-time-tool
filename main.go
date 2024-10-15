4e2a087e74b3246166fc337695030242cee829de
Thomas Koefod <devinthemtn@gmail.com>
2024-10-14 21:56:20 -0600

    created makefile for builds

--- main.go
+++ main.go
@@ -3,18 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
 	timeAlso := flag.Bool("t", false, "time")
 	nextTue := flag.Bool("tue", false, "next Tuesday")
+	showHelp := flag.Bool("h", false, "help")
 	// must be called after all flags are defined
 	flag.Parse()
 	// =========
+	if *showHelp {
+		fmt.Println("Utillity for showing formated time/date")
+		fmt.Println("-h shows this help command")
+		fmt.Println("-t shows the current date with the current time")
+		fmt.Println("-tue shows the date of the next tuesday from today")
+		os.Exit(0)
+	}
 	currentTime := time.Now()
 	if *timeAlso {
-		fmt.Println(currentTime.Format("2006-Oct-02(15:04:05)"))
+		fmt.Println(currentTime.Format("2006-Oct-02(15:04:05"))
 	} else if *nextTue {
 		nxtTue := nextTuesday(time.Now())
 		fmt.Println(nxtTue.Format("2006-Oct-02"))
---
+++ makefile
@@ -0,0 +1,21 @@
+BINARY_NAME=mytime
+
+.PHONY: build run test clean
+
+build: main.go
+		GOARCH=amd64 GOOS=darwin go build -o bin/${BINARY_NAME}-darwin main.go
+		GOARCH=amd64 GOOS=linux go build -o bin/${BINARY_NAME}-linux main.go
+		GOARCH=amd64 GOOS=windows go build -o bin/${BINARY_NAME}-windows main.go
+
+run: 
+		go run main.go
+		# ./${BINARY_NAME}
+
+test:
+		go test -v ./...
+
+clean:
+	go clean
+	rm bin/${BINARY_NAME}-darwin
+	rm bin/${BINARY_NAME}-linux
+	rm bin/${BINARY_NAME}-windows
